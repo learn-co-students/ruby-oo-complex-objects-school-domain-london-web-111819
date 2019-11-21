13d31e01cdb9127c3aca880ec4d23de429bb50a8
Sara Fotros <sarafotros@users.noreply.github.com>
2019-11-21 15:39:14 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  atrr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster[grade] || = []
+    @roster[grade].push(student)
+  end
+
+  def grade(num)
+    @roster[num]
+  end
+
+   def sort
+      @roster.each do |key, value|
+      value.sort!
+      end
+     @roster
+   end
+
+  end
+
+end
